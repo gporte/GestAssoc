e10f36a6326bd6da09d54548a0b502ff4bfc146c
gporte <guillaume.porte@gmail.com>
2014-02-04 17:37:14 +0100

    Initialisation du projet principal

---
+++ GestAssoc/GestAssoc/Bootstrapper.cs
@@ -0,0 +1,40 @@
+﻿using GestAssoc.Utility;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+using System.Windows;
+using System.Windows.Controls.Ribbon;
+
+namespace GestAssoc
+{
+	public class Bootstrapper : UnityBootstrapper
+	{
+		protected override System.Windows.DependencyObject CreateShell() {
+			return new ShellWindow();
+		}
+
+		protected override void InitializeShell() {
+			base.InitializeShell();
+			App.Current.MainWindow = (Window)this.Shell;
+			App.Current.MainWindow.Show();
+		}
+
+		protected override Microsoft.Practices.Prism.Modularity.IModuleCatalog CreateModuleCatalog() {
+			var moduleCatalog = new DirectoryModuleCatalog();
+			moduleCatalog.ModulePath = @".\Modules";
+
+			return moduleCatalog;
+		}
+
+		protected override Microsoft.Practices.Prism.Regions.RegionAdapterMappings ConfigureRegionAdapterMappings() {
+			var mappings = base.ConfigureRegionAdapterMappings();
+
+			if (mappings == null)
+				return null;
+
+			mappings.RegisterMapping(typeof(Ribbon), ServiceLocator.Current.GetInstance<RibbonRegionAdapter>());
+
+			return mappings;
+		}
+	}
+}
---
+++ GestAssoc/GestAssoc/ShellWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace GestAssoc
+{
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class ShellWindow : Window
+	{
+		public ShellWindow() {
+			InitializeComponent();
+		}
+	}
+}
