3a31d8724f087bd71676b1f49f2f1f5411904230
gporte <guillaume.porte@gmail.com>
2014-02-04 17:46:39 +0100

    ajout du projet Common

---
+++ GestAssoc/GestAssoc.Common/Constantes/RegionNames.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GestAssoc.Common.Constantes
+{
+	public static class RegionNames
+	{
+		public const string RibbonRegion = "RibbonRegion";
+		public const string ContentRegion = "ContentRegion";
+	}
+}
