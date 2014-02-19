c2a9a29967c92edaac4e97754eb76fc27343c255
gporte <guillaume.porte@gmail.com>
2014-02-20 00:19:50 +0100

    Ajout d'images pour les boutons du rubban gestion villes

--- GestAssoc/GestAssoc.Modules.GestionVilles/ViewModels/GestionVillesRibbonTabViewModel.cs
+++ GestAssoc/GestAssoc.Modules.GestionVilles/ViewModels/GestionVillesRibbonTabViewModel.cs
@@ -7,12 +7,12 @@ namespace GestAssoc.Modules.GestionVilles.ViewModels
 {
 	public class GestionVillesRibbonTabViewModel : ViewModelBase
 	{
-		public ICommand ShowConsultationVillesView { get; set; }
-		public ICommand ShowFormulaireVilleCreation { get; set; }
+		public ICommand ShowConsultationVillesCmd { get; set; }
+		public ICommand ShowFormulaireCreationVilleCmd { get; set; }
 
 		public GestionVillesRibbonTabViewModel() {
-			this.ShowConsultationVillesView = new ShowViewCommand(ViewNames.ConsultationVilles);
-			this.ShowFormulaireVilleCreation = new ShowViewCommand(ViewNames.FormulaireVille);
+			this.ShowConsultationVillesCmd = new ShowViewCommand(ViewNames.ConsultationVilles);
+			this.ShowFormulaireCreationVilleCmd = new ShowViewCommand(ViewNames.FormulaireVille);
 		}
 	}
 }
