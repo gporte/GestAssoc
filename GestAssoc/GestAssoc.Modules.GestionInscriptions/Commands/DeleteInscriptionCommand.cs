4449c8277ad5480ca7f0ded9a27cd1e41100736b
gporte <guillaume.porte@gmail.com>
2014-06-12 11:51:53 +0200

    loc+ recup element supprimé par erreur

---
+++ GestAssoc/GestAssoc.Modules.GestionInscriptions/Commands/DeleteInscriptionCommand.cs
@@ -0,0 +1,48 @@
+﻿using GestAssoc.Common.Commands;
+using GestAssoc.Common.Utility;
+using GestAssoc.Model.Models;
+using GestAssoc.Modules.GestionInscriptions.Constantes;
+using GestAssoc.Modules.GestionInscriptions.Properties;
+using GestAssoc.Modules.GestionInscriptions.Services;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using System;
+using System.Windows.Input;
+
+namespace GestAssoc.Modules.GestionInscriptions.Commands
+{
+	public class DeleteInscriptionCommand : ICommand
+	{
+		public bool CanExecute(object parameter) {
+			var itemToDelete = parameter as Adherent;
+
+			return itemToDelete != null
+				&& itemToDelete.Inscriptions.Count == 0;
+		}
+
+		public event EventHandler CanExecuteChanged {
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public void Execute(object parameter) {
+			var service = ServiceLocator
+				.Current.GetInstance<IUnityContainer>()
+				.Resolve<IGestionInscriptionsServices>();
+
+			var itemToDelete = parameter as Inscription;
+
+			try {
+				UIServices.SetBusyState();
+				service.DeleteInscription(itemToDelete);
+
+				NotificationHelper.WriteNotification(Resources.Log_EnregistrementSupprime);
+
+				new ShowViewCommand(ViewNames.ConsultationInscriptions.ToString()).Execute(null);
+			}
+			catch (Exception ex) {
+				NotificationHelper.ShowError(ex);
+			}
+		}
+	}
+}
--- GestAssoc/GestAssoc.Modules.GestionInscriptions/Properties/Resources.Designer.cs
+++ GestAssoc/GestAssoc.Modules.GestionInscriptions/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace GestAssoc.Modules.GestionInscriptions.Properties {
+    using System;
     
     
     /// <summary>
@@ -37,7 +38,7 @@ namespace GestAssoc.Modules.GestionInscriptions.Properties {
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GestAssoc.Modules.GestionInscriptions.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
@@ -56,6 +57,15 @@ namespace GestAssoc.Modules.GestionInscriptions.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Inscription supprimée..
+        /// </summary>
+        internal static string Log_EnregistrementSupprime {
+            get {
+                return ResourceManager.GetString("Log_EnregistrementSupprime", resourceCulture);
             }
         }
     }
